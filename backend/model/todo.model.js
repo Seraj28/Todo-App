1ef6b026c16e04b839065fcc46a93cb201ba6360
Seraj28 <seraj232855@gmail.com>
2025-08-24 23:12:12 +0530

    Project completed!!

---
+++ backend/model/todo.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";   // esko hath mat lagana ye pura completed hai
+
+const todoSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // referencing User model to connect to users collection in MongoDB.
+    required: true,
+  },
+});
+
+const Todo = mongoose.model("Todo", todoSchema);
+export default Todo;
+
+// esko hath mat lagana ye pura completed hai
